81acc241a36b68c396f57699f02a6275a6f2b2f3
Nnebue Desmond <46574948+sirdesmond09@users.noreply.github.com>
2022-03-30 17:48:09 +0100

    admin details

--- accounts/admin.py
+++ accounts/admin.py
@@ -2,4 +2,7 @@ from django.contrib import admin
 from accounts.models import User
 
 # Register your models here.
-admin.site.register(User)
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ["email", "role", "is_active", "is_admin"]
+    list_editable = ["role", "is_active", "is_admin"]
