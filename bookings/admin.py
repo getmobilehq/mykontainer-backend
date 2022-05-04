e0c6adde8d8e2659aa68d1c5d526b1f654b304cf
sirdesmond09 <sirdesmond09@gmail.com>
2022-05-04 15:57:34 +0100

    admin

--- bookings/admin.py
+++ bookings/admin.py
@@ -2,4 +2,8 @@ from django.contrib import admin
 from . models import Booking
 
 # Register your models here.
-admin.site.register(Booking)
+# admin.site.register(Booking)
+@admin.register(Booking)
+class BookingAdmin(admin.ModelAdmin):
+    list_display = ['id', "user", "is_active"]
+    list_editable = ("is_active",)
--- demurage/admin.py
+++ demurage/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import Demurage
 # Register your models here.
+
+
+@admin.register(Demurage)
+class DemurageAdmin(admin.ModelAdmin):
+    list_display = ['id', "shipping_company", "size","is_active"]
+    list_editable = ("is_active",)
