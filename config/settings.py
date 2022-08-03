40d88bcfdfaf1e8c56fbb2827f7af93770c0c654
sirdesmond09 <sirdesmond09@gmail.com>
2022-08-03 19:08:53 +0100

    add prod settings

--- config/settings.py
+++ config/settings.py
@@ -277,4 +277,24 @@ class Production(Staging):
     """
     The in-production settings.
     """
-    pass
+    DEBUG = False
+    ALLOWED_HOSTS = ["159.89.98.134"]
+    
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+    EMAIL_HOST = "smtp.mailgun.org"
+    EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+    EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+    EMAIL_PORT = 587 
+    EMAIL_USE_TLS = True  
+    
+    DATABASE ={
+        "default": {
+            "ENGINE": 'django.db.backends.postgresql_psycopg2',
+            "NAME": os.getenv("DB_NAME"),
+            "USER": "postgres",
+            "PASSWORD": os.getenv("DB_PASSWORD"),
+            "HOST": os.getenv("DB_HOST"),
+            "PORT": "5432"
+            }, 
+
+        }
